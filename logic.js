d2022e1451eb025b6e6d0382813a70cdbb8b4ae1
Amin-ur-Rahman <aminurar679799@gmail.com>
2025-08-21 16:40:11 +0600

    working on it

--- logic.js
+++ logic.js
@@ -1,43 +1,99 @@
+const checkbox = document.getElementById("toggle");
+const button = document.getElementById("orange-btn");
 
+checkbox.addEventListener("change", () => {
+  if (checkbox.checked) {
+    // console.log("checked");
+    button.classList.add("visual-effect");
+  } else {
+    button.classList.remove("visual-effect");
+  }
+});
 
-const checkbox = document.getElementById('toggle');
-const button = document.getElementById('orange-btn');
+const submitBtn = document.getElementById("submit-btn");
+const commentSection = document.getElementById("comment-box");
 
-checkbox.addEventListener('change', () => {
-    if (checkbox.checked) {
-        // console.log("checked");
-        button.classList.add('visual-effect');
+submitBtn.addEventListener("click", () => {
+  const commentTextArea = document.getElementById("comment-text-area");
+  const newComment = commentTextArea.value.trim();
 
-    } else {
-        button.classList.remove('visual-effect')
+  if (!newComment) {
+    alert("empty comments are not supported!");
+    return;
+  }
 
-    }
+  const newCommentBlock = document.createElement("p");
+  newCommentBlock.innerText = newComment;
+  newCommentBlock.setAttribute("class", "comments");
 
-})
+  commentSection.appendChild(newCommentBlock);
 
-const submitBtn = document.getElementById('submit-btn');
-const commentSection = document.getElementById('comment-box');
+  commentTextArea.value = "";
+});
 
+submitBtn.addEventListener("mouseenter", () => console.log("hello there"));
 
-submitBtn.addEventListener('click', () => {
+// console.log(itemsCollection);
 
-    const commentTextArea = document.getElementById('comment-text-area');
-    const newComment = commentTextArea.value.trim();
+// for (const item of itemsCollection) {
+//     item.addEventListener('click', () => {
+//         console.log(item);
 
+//     })
+// }
 
-    if (!newComment) {
-        alert("empty comments are not supported!")
-        return
-    };
+const orderedList = document.getElementById("ordered-list");
 
-    const newCommentBlock = document.createElement('p');
-    newCommentBlock.innerText = newComment;
-    newCommentBlock.setAttribute('class', 'comments')
+function clickUtility(parent, eventType, handler) {
+  // const itemsCollection = parent.children;
+  parent.addEventListener(eventType, handler);
+}
 
-    commentSection.appendChild(newCommentBlock);
+const classNameLi = document.querySelector(".items").getAttribute("class");
+console.log(classNameLi);
 
-    commentTextArea.value = '';
+clickUtility(orderedList, "click", function (event) {
+  console.log(event);
 
+  if (event.target.tagName === "LI") {
+    event.target.parentNode.removeChild(event.target);
+  }
+});
 
+const liItem = document.querySelector(".items").innerHTML;
 
-})
+const createBtn = document.getElementById("create-btn");
+createBtn.addEventListener("click", () => {
+  const newListItem = document.createElement("li");
+  newListItem.appendChild(document.createTextNode("new item"));
+
+  const orderedList = document.getElementById("ordered-list");
+  orderedList.appendChild(newListItem);
+  newListItem.setAttribute("class", classNameLi);
+});
+
+const secondItem = document.querySelector("li:nth-child(3)"); //- It means “select the <ol> element if it is the second child of its parent.”
+
+const anotherNewItem = document.createElement("li");
+anotherNewItem.appendChild(document.createTextNode("item77"));
+secondItem.replaceWith(anotherNewItem);
+anotherNewItem.setAttribute("class", classNameLi);
+let nextItem = anotherNewItem.nextElementSibling;
+
+while (nextItem) {
+  console.log(nextItem);
+  console.log(nextItem.nodeName);
+  nextItem = nextItem.nextElementSibling;
+}
+
+const buttons = document.querySelectorAll(".button");
+const body = document.getElementById("body");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let colorId = e.target.id;
+    console.log(colorId);
+
+    body.setAttribute("class", colorId);
+  });
+});
